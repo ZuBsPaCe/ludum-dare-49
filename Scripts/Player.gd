f788f799fd31f772f9dcc94acb0015df8eeb1c2d
ZuBsPaCe <kurt.rudin@gmx.net>
2021-10-02 10:11:10 +0200

    Added weapon to player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,14 @@ extends KinematicBody2D
 
 export var speed := 64.0
 
+onready var _weapon_right := $WeaponRight
+onready var _weapon_left := $WeaponLeft
+onready var _center := $Center
+
 
 var _target_velocity := Vector2.ZERO
 
 var _shoot_cooldown := Cooldown.new()
-
 
 func _ready():
 	_shoot_cooldown.setup(self, 0.5, true)
@@ -33,14 +36,32 @@ func _process(_delta):
 		_target_velocity.x -= 1
 
 	_target_velocity = _target_velocity.normalized() * speed
+	
+	var weapon
+	var base_angle:Vector2
+	
+	var global_mouse_pos = get_global_mouse_position()
+	if Vector2.UP.angle_to(global_mouse_pos - _center.global_position) >= 0.0:
+		_weapon_left.visible = false
+		weapon = _weapon_right
+		base_angle = Vector2.RIGHT
+	else:
+		_weapon_right.visible = false
+		weapon = _weapon_left
+		base_angle = Vector2.LEFT
+		
+	weapon.visible = true
+	
 
+	var weapon_dir:Vector2 = (get_global_mouse_position() - weapon.global_position).normalized()
+	weapon.rotation = base_angle.angle_to(weapon_dir)
 
 	if Input.is_action_pressed("left_click") && _shoot_cooldown.done:
 		_shoot_cooldown.restart()
 		
-		var dir := (get_global_mouse_position() - position).normalized()
-		Globals.shake(dir)
-		Globals.create_bullet(position, dir)
+		
+		Globals.shake(weapon_dir)
+		Globals.create_bullet(weapon.global_position, weapon_dir)
 
 
 func _physics_process(_delta):
