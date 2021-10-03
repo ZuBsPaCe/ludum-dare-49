00a51112f3f250cd3e5f29eabaafe8720c33d0f4
ZuBsPaCe <kurt.rudin@gmx.net>
2021-10-03 23:49:25 +0200

    TitleScreen

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -4,9 +4,27 @@ onready var title := $Title
 onready var title_scale_tween := $Title/TitleScaleTween
 onready var title_rotationtween := $Title/TitleRotationTween
 
+onready var title_player_tween := $PlayerTitle/TweenPlayer
+onready var title_monster1_tween := $TitleMonster1/TweenMonster1
+onready var title_monster2_tween := $TitleMonster2/TweenMonster2
+onready var title_monster3_tween := $TitleMonster3/TweenMonster3
+
+onready var title_player := $PlayerTitle
+onready var title_monster1 := $TitleMonster1
+onready var title_monster2 := $TitleMonster2
+onready var title_monster3 := $TitleMonster3
+
+
+var player_pos
+var monster1_pos
+var monster2_pos
+var monster3_pos
 
 func _ready():
-	pass
+	player_pos = title_player.position
+	monster1_pos = title_monster1.position
+	monster2_pos = title_monster2.position
+	monster3_pos = title_monster3.position
 
 
 func _process(delta):
@@ -15,6 +33,18 @@ func _process(delta):
 		
 	if !title_rotationtween.is_active():
 		_start_rotation_tween()
+		
+	if !title_player_tween.is_active():
+		_start_tween(title_player_tween, title_player, player_pos)
+		
+	if !title_monster1_tween.is_active():
+		_start_tween(title_monster1_tween, title_monster1, monster1_pos)
+		
+	if !title_monster2_tween.is_active():
+		_start_tween(title_monster2_tween, title_monster2, monster2_pos)
+		
+	if !title_monster3_tween.is_active():
+		_start_tween(title_monster3_tween, title_monster3, monster3_pos)
 		
 func _start_scale_tween():
 	var scale_factor = 0.8
@@ -80,5 +110,23 @@ func _start_rotation_tween():
 			
 	# warning-ignore:return_value_discarded
 	title_scale_tween.start()
+	
+	
+func _start_tween(tween, obj, pos):
+
+	pos.x = pos.x + 10.0 - randf() * 20
+	pos.y = pos.y + 10.0 - randf() * 20
+	
+	tween.interpolate_property(
+			obj,
+			"position",
+			obj.position,
+			pos,
+			0.3 + randf(),
+			Tween.TRANS_SINE, 
+			Tween.EASE_IN_OUT)
+			
+	# warning-ignore:return_value_discarded
+	tween.start()
 	
 	
